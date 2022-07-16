e7581fcde08bb35db2322a107339697a4ad3ea35
jerichosy <69558553+jerichosy@users.noreply.github.com>
2022-07-17 01:17:12 +0800

    Working yt-dlp extract info API endpoint

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import yt_dlp
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/extract")
+def extract(url: str):
+    ydl_opts = {}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        return ydl.sanitize_info(info_dict)
